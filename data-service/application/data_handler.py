bf5650ccd6d73d107a8541f8afdb0824292d98df
yuk-kei <yukkei_mh@outlook.com>
2023-08-01 12:18:03 -0700

    unify the time zone

--- data-service/application/data_handler.py
+++ data-service/application/data_handler.py
@@ -1,7 +1,7 @@
 import os
-from datetime import datetime
 
 from influxdb_client.client import influxdb_client
+from dateutil.parser import parse
 
 
 class InfluxDataHandler:
@@ -60,7 +60,6 @@ class InfluxDataHandler:
 
 
 def time_or_time_delta(curr_time_str):
-
     if len(curr_time_str) == 1:
         return "Invalid time format"
     if curr_time_str[-1] == 'd':
@@ -72,42 +71,39 @@ def time_or_time_delta(curr_time_str):
     elif curr_time_str[-1] == 's':
         return curr_time_str
 
-
     try:
-        target_time = datetime.strptime(curr_time_str, '%Y-%m-%dT%H:%M:%S')
-        # target_time += utc_offset
+        target_time = parse(curr_time_str)
+
         target_time_seconds = int(target_time.timestamp())
         return target_time_seconds
 
     except ValueError:
-        try:
-            target_time = datetime.strptime(curr_time_str, '%Y-%m-%dT%H:%M')
-
-            target_time_seconds = int(target_time.timestamp())
-            return target_time_seconds
-        except ValueError:
-            print(f"Error: unable to parse time string {curr_time_str}")
-
-
+        print(f"Error: unable to parse time string {curr_time_str}")
 
 
 """test code"""
-input_time1 = "2023-07-31T15:30"
-output1 = time_or_time_delta(input_time1)
-print(output1)
+
+# input_time1 = "2023-07-31T20:30:00.000Z"
+# input_time2 = "2023-07-31T20:34:00.000Z"
+# output1 = time_or_time_delta(input_time1)
+# output2 = time_or_time_delta(input_time2)
+# print(output1)
+# print(output2)
 # client = influxdb_client.InfluxDBClient(
 #     url="http://128.195.151.182:8086",
 #     token="FzxLoZXd06eIEYzueFsX1Kj21w5LwClTr4TC0w6NrWhzuBqeAVl0Sb9Nqiut5HRNZqcHgIzd0CalUl1__AynLw==",
 #     org="calit2"
 # )
 # query_api = client.query_api()
-# tables = query_api.query_data_frame('from(bucket:"sensor_data") |> range(start: -20h)')
+# tables = query_api.query_data_frame(f'from(bucket:"sensor_data") |> range(start: {output1}, stop:{output2})')
 #
-# df = query_api.query_data_frame('from(bucket:"sensor_data") |> range(start: -21h) '
+# df = query_api.query_data_frame(f'from(bucket:"sensor_data") |> range(start: -21h) '
 #                                 '|> pivot(rowKey:["_time"], columnKey: ["_field"], valueColumn: "_value") '
 #                                 '|> keep(columns: ["time", "values"])')
-# print(tables.to_string())
-# for table in tables:
+# query = f'from(bucket:"sensor_data") |> range(start: {output1}, stop:{output2})'
+# result = query_api.query(query)
+# print(result)
+# for table in result:
 #
 #     for record in table.records:
 #
